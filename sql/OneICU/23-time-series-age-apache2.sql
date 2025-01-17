812d913c7d9153e9ccffc9aaa0816db085c4b1cc
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-01-17 11:35:29 +0900

    Update and rename 23-time-series-age-los.sql to 23-time-series-age-apache2.sql

--- sql/OneICU/23-time-series-age-apache2.sql
+++ sql/OneICU/23-time-series-age-apache2.sql
@@ -13,33 +13,26 @@ with
         from `medicu-beta.latest_one_icu_derived.extended_icu_stays`
         where age is not null
     ),
-    los_prep as (
-        select
-            icu_admission_year,
-            round(
-                cast(timestamp_diff(out_time, in_time, hour) as int64) / 24, 1
-            ) as icu_los
-        from `medicu-beta.latest_one_icu_derived.extended_icu_stays`
-        where out_time is not null
-    ),
-    los_stats as (
+    apache2_stats as (
         select distinct
             icu_admission_year,
-            'los' as field_name,
-            percentile_cont(icu_los, 0.5) over (
+            'apache2' as field_name,
+            percentile_cont(apache2_score, 0.5) over (
                 partition by icu_admission_year
             ) as median,
-            percentile_cont(icu_los, 0.25) over (
+            percentile_cont(apache2_score, 0.25) over (
                 partition by icu_admission_year
             ) as percentile_25,
-            percentile_cont(icu_los, 0.75) over (
+            percentile_cont(apache2_score, 0.75) over (
                 partition by icu_admission_year
             ) as percentile_75
-        from los_prep
+        from `medicu-beta.latest_one_icu_derived.apache2`
+        inner join
+            `medicu-beta.latest_one_icu_derived.extended_icu_stays` using (icu_stay_id)
     )
 select *
 from age_stats
 union all
 select *
-from los_stats
+from apache2_stats
 order by field_name, icu_admission_year
