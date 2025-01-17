455c03f7ef8c0133548052dc4f183165c78c22b7
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-01-17 12:15:24 +0900

    Update and rename 06-apache-2-and-3.sql to 06-aps-ii-and-iii.sql

--- sql/OneICU/06-aps-ii-and-iii.sql
+++ sql/OneICU/06-aps-ii-and-iii.sql
@@ -1,16 +1,16 @@
 with
-    apache2_stats as (
+    apsii_stats as (
         select distinct
-            'apache2_score' as field_name,
-            percentile_cont(apache2_score, 0.5) over () as median,
-            percentile_cont(apache2_score, 0.25) over () as percentile_25,
-            percentile_cont(apache2_score, 0.75) over () as percentile_75
+            'apsii' as field_name,
+            percentile_cont(apsii, 0.5) over () as median,
+            percentile_cont(apsii, 0.25) over () as percentile_25,
+            percentile_cont(apsii, 0.75) over () as percentile_75
         from `medicu-beta.latest_one_icu_derived.apache2`
-        where apache2_score is not null
+        where apsii is not null
     ),
-    apache2_missing as (
+    apsii_missing as (
         select
-            'apache2_score' as field_name,
+            'apsii' as field_name,
             (
                 select count(distinct icu_stay_id)
                 from `medicu-beta.latest_one_icu_derived.extended_icu_stays`
@@ -31,20 +31,20 @@ with
                 1
             ) as proportion_missing
         from `medicu-beta.latest_one_icu_derived.apache2`
-        where apache2_score is not null
+        where apsii is not null
     ),
-    apache3_stats as (
+    apsiii_stats as (
         select distinct
-            'apache3_score' as field_name,
-            percentile_cont(apache3_score, 0.5) over () as median,
-            percentile_cont(apache3_score, 0.25) over () as percentile_25,
-            percentile_cont(apache3_score, 0.75) over () as percentile_75
+            'apsiii' as field_name,
+            percentile_cont(apsiii, 0.5) over () as median,
+            percentile_cont(apsiii, 0.25) over () as percentile_25,
+            percentile_cont(apsiii, 0.75) over () as percentile_75
         from `medicu-beta.latest_one_icu_derived.apache3`
-        where apache3_score is not null
+        where apsiii is not null
     ),
-    apache3_missing as (
+    apsiii_missing as (
         select
-            'apache3_score' as field_name,
+            'apsiii' as field_name,
             (
                 select count(distinct icu_stay_id)
                 from `medicu-beta.latest_one_icu_derived.extended_icu_stays`
@@ -65,12 +65,12 @@ with
                 1
             ) as proportion_missing
         from `medicu-beta.latest_one_icu_derived.apache3`
-        where apache3_score is not null
+        where apsiii is not null
     )
 select field_name, median, percentile_25, percentile_75, n_missing, proportion_missing
-from apache2_stats
-inner join apache2_missing using (field_name)
+from apsii_stats
+inner join apsii_missing using (field_name)
 union all
 select field_name, median, percentile_25, percentile_75, n_missing, proportion_missing
-from apache3_stats
-inner join apache3_missing using (field_name)
+from apsiii_stats
+inner join apsiii_missing using (field_name)
