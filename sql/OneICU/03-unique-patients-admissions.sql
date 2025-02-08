658c12066765f885ae582258b843799d9d7dfd65
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-02-08 21:59:41 +0900

    Update 03-unique-patients-admissions.sql

--- sql/OneICU/03-unique-patients-admissions.sql
+++ sql/OneICU/03-unique-patients-admissions.sql
@@ -1,4 +1,4 @@
 select
     count(distinct subject_id) as unique_patients,
     count(distinct icu_stay_id) as unique_admissions,
-from `medicu-beta.latest_one_icu_derived.extended_icu_stays`
+from `medicu-beta.snapshots_one_icu_derived.extended_icu_stays_20250206`
