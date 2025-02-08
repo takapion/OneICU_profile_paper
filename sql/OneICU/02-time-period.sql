804e8597fe507c7df0b44cbae3205e67697dc87f
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-02-08 21:59:05 +0900

    Update 02-time-period.sql

--- sql/OneICU/02-time-period.sql
+++ sql/OneICU/02-time-period.sql
@@ -1,4 +1,4 @@
 select
   min(icu_admission_year) as start_year_of_data_collection,
   max(icu_admission_year) as end_year_of_data_collection
-from `medicu-beta.latest_one_icu_derived.extended_icu_stays`
+from `medicu-beta.snapshots_one_icu_derived.extended_icu_stays_20250206`
