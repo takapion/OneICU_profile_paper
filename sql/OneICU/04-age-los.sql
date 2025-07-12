c81eeb4a469b3cf7c96da7b1cc7cb7462c3bb26e
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-07-12 11:16:44 +0900

    Update 04-age-los.sql

--- sql/OneICU/04-age-los.sql
+++ sql/OneICU/04-age-los.sql
@@ -6,7 +6,7 @@ with
             percentile_cont(age, 0.25) over () as percentile_25,
             percentile_cont(age, 0.75) over () as percentile_75
         from `medicu-biz.latest_one_icu_derived.extended_icu_stays`
-        where age is not null
+        where age is not null and icu_admission_year <= 2024
     ),
     age_missing as (
         select
@@ -14,6 +14,7 @@ with
             countif(age is null) as n_missing,
             round(100 * countif(age is null) / count(*), 1) as proportion_missing
         from `medicu-biz.latest_one_icu_derived.extended_icu_stays`
+        where icu_admission_year <= 2024
     ),
     los_prep as (
         select
@@ -21,7 +22,7 @@ with
                 cast(timestamp_diff(out_time, in_time, hour) as int64) / 24, 1
             ) as icu_los
         from `medicu-biz.latest_one_icu_derived.extended_icu_stays`
-        where out_time is not null
+        where out_time is not null and icu_admission_year <= 2024
     ),
     los_stats as (
         select distinct
@@ -37,6 +38,7 @@ with
             countif(out_time is null) as n_missing,
             round(100 * countif(out_time is null) / count(*), 1) as proportion_missing
         from `medicu-biz.latest_one_icu_derived.extended_icu_stays`
+        where icu_admission_year <= 2024
     )
 select field_name, median, percentile_25, percentile_75, n_missing, proportion_missing
 from age_stats
