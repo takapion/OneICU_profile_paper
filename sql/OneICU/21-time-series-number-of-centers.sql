1b7e3698388bfa464dce5101b2a3c7b319ffa5ca
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-07-13 00:15:16 +0900

    Update 21-time-series-number-of-centers.sql

--- sql/OneICU/21-time-series-number-of-centers.sql
+++ sql/OneICU/21-time-series-number-of-centers.sql
@@ -1,4 +1,5 @@
 select icu_admission_year, count(distinct hospital_id) as number_of_centers
 from `medicu-biz.latest_one_icu_derived.extended_icu_stays`
+where icu_admission_year <= 2024
 group by icu_admission_year
 order by icu_admission_year
