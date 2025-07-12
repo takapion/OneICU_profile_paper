ca1e4a5fc642df1e002240ca6e4536e3ed7deb09
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-07-12 11:09:22 +0900

    Update 03-unique-patients-admissions.sql

--- sql/OneICU/03-unique-patients-admissions.sql
+++ sql/OneICU/03-unique-patients-admissions.sql
@@ -2,3 +2,4 @@ select
     count(distinct subject_id) as unique_patients,
     count(distinct icu_stay_id) as unique_admissions,
 from `medicu-biz.latest_one_icu_derived.extended_icu_stays`
+where icu_admission_year <= 2024
