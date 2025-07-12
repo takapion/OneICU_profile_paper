ed0635e97b24726ef991ad9203ea9378cdb9a933
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-07-13 08:56:12 +0900

    Update 08-disease-category.sql

--- sql/OneICU/08-disease-category.sql
+++ sql/OneICU/08-disease-category.sql
@@ -8,29 +8,22 @@ with recategorize as (
       when category = 'burn' then 'toxicological_environmental_disorders'
       when category = 'temperature_disorder' then 'toxicological_environmental_disorders'
       when category = 'hanging_asphyxiation' then 'toxicological_environmental_disorders'
-      when category = 'null' then null
+      when category = 'null' then 'others'
+      when category is null then 'others'
       else category
       end as category
-  from `medicu-biz.latest_one_icu_derived.extended_icu_diagnoses`
-  where primary
-),
-diag_cat as (
-  select
-    category,
-    count(*) as count,
-  from recategorize
-  where category is not null
-  group by category
-),
-n_patients_with_diag as (
-  select count(distinct icu_stay_id) as n_patients
-  from recategorize
-  where category is not null
+  from `medicu-biz.latest_one_icu_derived.extended_icu_stays`
+  left join (
+    select icu_stay_id, category
+    from `medicu-biz.latest_one_icu_derived.unioned_icu_diagnoses`
+    where primary
+  ) using(icu_stay_id)
+  where icu_admission_year <= 2024
 )
 select
   category,
-  count,
-  round(100 * count / n_patients, 1) as proportion,
-from diag_cat
-cross join n_patients_with_diag
-order by count desc
+  count(*) as count,
+  round(count(*) * 100 / sum(count(*)) over(), 1) as proportion
+from recategorize
+group by category
+order by proportion desc
