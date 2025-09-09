032b238d141bfe4158cd5141a7ff3401b3a851eb
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-09-09 13:22:17 +0900

    Update 09-medical-device-use.sql

--- sql/OneICU/09-medical-device-use.sql
+++ sql/OneICU/09-medical-device-use.sql
@@ -51,24 +51,6 @@ with
         inner join `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716` using(icu_stay_id)
         where icu_admission_year <= 2024
     ),
-    irrt as (
-        select
-            'irrt' as field_name,
-            count(distinct icu_stay_id) as count,
-            round(
-                100
-                * count(distinct icu_stay_id)
-                / (
-                    select count(*)
-                    from `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716`
-                    where icu_admission_year <= 2024
-                ),
-                1
-            ) as proportion
-        from `medicu-biz.snapshots_one_icu.renal_replacement_therapy_20250716`
-        inner join `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716` using(icu_stay_id)
-        where type in ('hd', 'ecum', 'sled') and icu_admission_year <= 2024
-    ),
     crrt as (
         select
             'crrt' as field_name,
@@ -86,24 +68,6 @@ with
         from `medicu-biz.snapshots_one_icu.renal_replacement_therapy_20250716`
         inner join `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716` using(icu_stay_id)
         where type in ('chdf', 'crrt', 'chd', 'chf') and icu_admission_year <= 2024
-    ),
-    pe as (
-        select
-            'pe' as field_name,
-            count(distinct icu_stay_id) as count,
-            round(
-                100
-                * count(distinct icu_stay_id)
-                / (
-                    select count(*)
-                    from `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716`
-                    where icu_admission_year <= 2024
-                ),
-                1
-            ) as proportion
-        from `medicu-biz.snapshots_one_icu.renal_replacement_therapy_20250716`
-        inner join `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716` using(icu_stay_id)
-        where type in ('pe') and icu_admission_year <= 2024
     )
 select *
 from mv
@@ -115,10 +79,5 @@ select *
 from hfo
 union all
 select *
-from irrt
-union all
-select *
 from crrt
-union all
-select *
-from pe
+order by field_name
