0fa85ac173464880975937bb5114c55ab3873312
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2025-09-09 14:57:33 +0900

    Create 15-initial-norepinephrine-dose.sql

---
+++ sql/OneICU/15-initial-norepinephrine-dose.sql
@@ -0,0 +1,39 @@
+with
+    weights as (
+        select
+            icu_stay_id,
+            body_weight_imputed as body_weight,
+            percentile_cont(body_weight_imputed, 0.5) over (
+                partition by female
+            ) as median_by_gender
+        from `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716`
+    ),
+    weights_imputed as (
+        select icu_stay_id, coalesce(body_weight, median_by_gender) as body_weight
+        from weights
+    ),
+    ne as (
+        select
+            icu_stay_id,
+            min_by(unit_per_hour, start_time) as initial_norepinephrine_rate
+        from
+            `medicu-biz.snapshots_one_icu_derived.infusion_injection_active_ingredient_rate_20250716`
+        where active_ingredient_name = 'noradrenaline'
+        group by icu_stay_id
+    ),
+    ne_standardized as (
+        select
+            icu_stay_id,
+            round(
+                initial_norepinephrine_rate / (body_weight * 0.06), 3
+            ) as initial_norepinephrine_rate_standardized
+        from ne
+        inner join weights_imputed using (icu_stay_id)
+    )
+select distinct
+    percentile_cont(initial_norepinephrine_rate_standardized, 0.5) over () as median,
+    percentile_cont(initial_norepinephrine_rate_standardized, 0.25) over (
+    ) as percentile_25,
+    percentile_cont(initial_norepinephrine_rate_standardized, 0.75) over (
+    ) as percentile_75
+from ne_standardized
