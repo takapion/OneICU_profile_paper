96c186e87614d440a9829265d12b5ec48a66a8af
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2026-01-01 21:37:49 +0900

    Update 22-time-series-unique-patients-admissions.sql

--- sql/OneICU/22-time-series-unique-patients-admissions.sql
+++ sql/OneICU/22-time-series-unique-patients-admissions.sql
@@ -2,7 +2,6 @@ select
     icu_admission_year,
     count(distinct icu_stay_id) as unique_admissions,
     count(distinct subject_id) as unique_patients
-from `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716`
-where icu_admission_year <= 2024
+from `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20251228`
 group by icu_admission_year
 order by icu_admission_year
