f4db9de0cc3a22ba058f288a608bd25db602a307
Takahiro Kinoshita Kiritoshi <54335492+takapion@users.noreply.github.com>
2026-01-01 21:36:00 +0900

    Update 14-vasopressor-use.sql

--- sql/OneICU/14-vasopressor-use.sql
+++ sql/OneICU/14-vasopressor-use.sql
@@ -6,18 +6,15 @@ select
         * count(distinct icu_stay_id)
         / (
             select count(distinct icu_stay_id)
-            from `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716`
-            where icu_admission_year <= 2024
+            from `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20251228`
         ),
         1
     ) as proportion
 from
-    `medicu-biz.snapshots_one_icu_derived.infusion_injection_active_ingredient_rate_smoothed_20250716`
+    `medicu-biz.snapshots_one_icu_derived.infusion_injection_active_ingredient_rate_smoothed_20251228`
 inner join
-    `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20250716` using (
+    `medicu-biz.snapshots_one_icu_derived.extended_icu_stays_20251228` using (
         icu_stay_id
     )
 where
-    icu_admission_year <= 2024
-    and active_ingredient_name
-    in ('dopamine', 'noradrenaline', 'adrenaline', 'phenylephrine', 'vasopressin')
+    active_ingredient_name in ('dopamine', 'noradrenaline', 'adrenaline', 'phenylephrine', 'vasopressin')
